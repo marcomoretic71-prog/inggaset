8302b3d500272593c441c7b74dd74c1f2b4f80f1
Marco Moretic <marcomoretic71@gmail.com>
2026-09-12 19:54:50 -0300

    Arregla migraciones duplicadas y carga 6 responsables

--- caja/migrations/0003_cargar_personas.py
+++ caja/migrations/0003_cargar_personas.py
@@ -2,16 +2,13 @@ from django.db import migrations
 
 def cargar_personas(apps, schema_editor):
     Persona = apps.get_model('caja', 'Persona')
-    nombres = ['Diego', 'Tula', 'Rafa', 'Fede', 'Claudio', 'Puyo']
-    for nombre in nombres:
+    for nombre in ['Diego', 'Tula', 'Rafa', 'Fede', 'Claudio', 'Puyo']:
         Persona.objects.get_or_create(nombre=nombre)
 
 class Migration(migrations.Migration):
-
     dependencies = [
-        ('caja', '0001_initial'),
+        ('caja', '0002_persona_alter_movimientocaja_descripcion_and_more'),
     ]
-
     operations = [
-        migrations.RunPython(cargar_personas),
+        migrations.RunPython(cargar_personas, migrations.RunPython.noop),
     ]

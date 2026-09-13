b640d1296f14ddbdd3a79496f515ea5d575b305c
Marco Moretic <marcomoretic71@gmail.com>
2026-09-12 22:52:38 -0300

    Fix caja route

--- config/urls.py
+++ config/urls.py
@@ -3,5 +3,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('gestion.urls')), # <-- Agrega esta línea
+    path('', include('gestion.urls')),
+    path('caja/', include('caja.urls')),  # <-- AGREGA ESTA
 ]
